371390ec706e3d2a8aad855cbd74c969d992d11f
Dylan Anctil <134968796+danctila@users.noreply.github.com>
2024-12-30 16:28:11 -0500

    Initial Commit

---
+++ src/App.jsx
@@ -0,0 +1,9 @@
+function App() {
+  return (
+    <div className="h-screen bg-blue-500 flex justify-center items-center text-white">
+      <h1 className="text-3xl font-bold">Hello, Tailwind!</h1>
+    </div>
+  );
+}
+
+export default App;
