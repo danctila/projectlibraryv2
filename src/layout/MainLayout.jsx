0d3e1e009424b92650070dedaaca5c961bd6e261
Dylan Anctil <134968796+danctila@users.noreply.github.com>
2024-12-31 16:33:32 -0500

    feat: add global next page overlay

--- src/layout/MainLayout.jsx
+++ src/layout/MainLayout.jsx
@@ -1,11 +1,20 @@
-import { useState, useEffect, useRef } from "react";
+import { useState, useEffect } from "react";
 import { useLocation } from "react-router-dom";
 import Navigation from "../components/Navigation";
 import Loader from "../components/Loader";
+import { useScroll } from "../Context/ScrollContext";
+
+const sectionTextMap = {
+  profile: "PROJECTS",
+  projects: "BLOG",
+  blog: "CONTACT",
+  contact: null,
+};
 
 export default function MainLayout({ children, navbarType }) {
   const location = useLocation();
   const [loading, setLoading] = useState(false);
+  const { activeSection } = useScroll();
   const isHomePage = location.pathname === "/";
 
   useEffect(() => {
@@ -45,17 +54,40 @@ export default function MainLayout({ children, navbarType }) {
     return () => window.removeEventListener("hashchange", handleHashChange);
   }, [isHomePage]);
 
+  const nextSectionText = sectionTextMap[activeSection];
+
   return (
     <div className="relative w-screen h-screen overflow-hidden">
       <Navigation />
       {loading ? <Loader /> : children}
+      {/* Global Overlay */}
+      {nextSectionText && (
+        <div className="fixed bottom-0 w-full flex flex-col items-center">
+          <p className="font-neue text-[20px] font-normal text-[#262329] dark:text-[#FBFBFB] mb-2">
+            {nextSectionText}
+          </p>
+          <svg
+            version="1.1"
+            xmlns="http://www.w3.org/2000/svg"
+            viewBox="0 0 150 150"
+            style={{ width: "29px", height: "29px" }}
+            xmlSpace="preserve"
+            className="fill-[#262329] dark:fill-[#FBFBFB]"
+          >
+            <polygon
+              id="XMLID_818_"
+              points="0,0 0,150 30.002,150 30.002,60 120.669,60 120.669,150 150,150 150,0"
+            />
+          </svg>
+        </div>
+      )}
     </div>
-    // for different nav bar layouts
-    //   <div>
-    //   {navbarType === "home" && <HomeNavBar />}
-    //   {navbarType === "blog" && <BlogNavBar />}
-    //   {navbarType === "project" && <ProjectNavBar />}
-    //   <main>{children}</main>
-    // </div>
   );
 }
+// for different nav bar layouts
+//   <div>
+//   {navbarType === "home" && <HomeNavBar />}
+//   {navbarType === "blog" && <BlogNavBar />}
+//   {navbarType === "project" && <ProjectNavBar />}
+//   <main>{children}</main>
+// </div>
