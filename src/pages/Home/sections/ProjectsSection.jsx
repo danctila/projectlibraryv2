3cd839f2cb4362cff71a798d3da8d7662734a6db
Dylan Anctil <134968796+danctila@users.noreply.github.com>
2025-01-04 15:34:49 -0500

    feat: add see all projects button

--- src/pages/Home/sections/ProjectsSection.jsx
+++ src/pages/Home/sections/ProjectsSection.jsx
@@ -84,15 +84,23 @@ export default function ProjectsSection() {
 
   return (
     <section className="min-h-screen snap-start bg-[#FBFBFB] dark:bg-[#262329] font-neue flex items-center justify-center">
-      <div className="max-w-[1915px] mx-auto px-[30px] tablet:px-[100px] desktop:px-[130px] w-full">
+      <div className="max-w-[1915px] mx-auto px-[30px] tablet:px-[100px] desktop:px-[130px] w-full relative">
         {/* Section Header */}
-        <div className="mb-10">
+        <div className="mb-10 ">
           <h1 className="text-[40px] font-normal text-[#262329] dark:text-white">
             Projects
           </h1>
-          <p className="text-[20px] text-[#645E6E] dark:text-[#D8D6DC] leading-[25px] w-[310px] tablet:w-[450px] desktop:w-[450px]">
-            Explore some of the exciting projects I've worked on recently.
-          </p>
+          <div className="relative w-full flex flex-col tablet:flex-row items-start tablet:items-end justify-between">
+            <p className="text-[20px] text-[#645E6E] dark:text-[#D8D6DC] leading-[25px] w-[310px] tablet:w-[450px] desktop:w-[450px]">
+              Explore some of the exciting projects I've worked on recently.
+            </p>
+            <Link
+              to="/projects"
+              className="mt-6 tablet:mt-4 desktop:mt-0 text-[#645E6E] dark:text-[#D8D6DC] hover:text-black dark:hover:text-white text-[20px] underline self-start tablet:self-auto"
+            >
+              See all projects
+            </Link>
+          </div>
         </div>
 
         {/* Carousel Container */}
