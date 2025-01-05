19e5bff753d05ede7934e58a8451700e8b86c607
Dylan Anctil <134968796+danctila@users.noreply.github.com>
2025-01-04 20:26:24 -0500

    feat: add headshot image to profile

--- src/pages/Home/sections/ProfileSection.jsx
+++ src/pages/Home/sections/ProfileSection.jsx
@@ -1,12 +1,13 @@
 import { AnimatePresence, motion } from "framer-motion";
 import { useScroll } from "../../../Context/ScrollContext";
+import headshot from "../../../assets/headshot.webp";
 
 export default function ProfileSection() {
   const { activeSection } = useScroll();
 
   return (
     <div className="relative min-h-screen flex flex-col justify-center items-center bg-[#FBFBFB] dark:bg-[#262329]">
-      <div className="flex flex-col desktop:flex-row items-center desktop:items-start desktop:space-x-8">
+      <div className="flex flex-col desktop:flex-row items-center desktop:items-center space-y-[50px] mobile:space-y-[50px] tablet:space-y-[50px] desktop:space-x-[100px]">
         {/* Text Section */}
         <div className="flex flex-col items-start text-left w-[310px] tablet:w-[450px] desktop:w-[450px]">
           <p className="font-neue text-[40px] font-normal text-[#262329] dark:text-white leading-[50px]">
@@ -43,9 +44,18 @@ export default function ProfileSection() {
             something meaningful.
           </p>
         </div>
-        {/* Image Section */}
+        {/* Image + Border Section */}
         <div className="mt-6 tablet:mt-8 desktop:mt-0">
-          <div className="bg-gray-300 dark:bg-gray-600 w-[270px] h-[270px] mobile:w-[270px] mobile:h-[270px] tablet:w-[430px] tablet:h-[430px] desktop:w-[430px] desktop:h-[430px]"></div>
+          <div className="relative w-[270px] h-[270px] mobile:w-[270px] mobile:h-[270px] tablet:w-[400px] tablet:h-[400px] desktop:w-[400px] desktop:h-[400px]">
+            {/* Border Box */}
+            <div className="absolute -top-2 -left-2 w-full h-full border-[1px] border-[#6E07F3] dark:border-[#8A34F9]" />
+            {/* Image */}
+            <img
+              src={headshot}
+              alt="headshot"
+              className="bg-gray-300 dark:bg-gray-600 w-full h-full"
+            />
+          </div>
         </div>
       </div>
     </div>
